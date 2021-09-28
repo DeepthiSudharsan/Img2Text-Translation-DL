3fadf3a94ff50b537774b7695971b270577c371f
Deepthi-S <59824729+DeepthiSudharsan@users.noreply.github.com>
2021-09-28 22:33:08 +0530

    Add files via upload

---
+++ Img-Text-Translation-Streamlit/img2text_translate.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from multipage import MultiPage
+from pages import img_translate, text_translate ,welcome # import your pages here
+
+# Adding an image to the side bar 
+st.sidebar.image("https://rb.gy/2wwoq1", width=None)
+
+# Create an instance of the app 
+app = MultiPage()
+# Add all your application here
+app.add_page("Welcome - Namaste - Vanakkam",welcome.app)
+app.add_page("Translate text from Images", img_translate.app)
+app.add_page("Translate text", text_translate.app)
+
+# The main app
+app.run()
+
+st.sidebar.subheader("Respective Github Repo : ")
+st.sidebar.markdown("[![Github](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJGtP-Pq0P67Ptyv3tB7Zn2ZYPIT-lPGI7AA&usqp=CAU)](https://github.com/DeepthiSudharsan/Img2Text-Translation-DL)")
